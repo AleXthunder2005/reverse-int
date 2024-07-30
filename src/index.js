04a6327b506c1f514754dfb87b4d4fcde326779e
AleXthunder2005 <35100077@study.bsuir.by>
2024-07-30 23:59:23 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let newN = 0;
+    n = Math.abs(n);
+    while (n > 0) {
+        newN *= 10;
+        newN += n % 10;
+        n = Math.trunc(n / 10);
+    }
+    return newN;
 }
